a10bb53dd1b25c4e1b5357743008532e36342a46
Marc Hughes <mhughes@tomsnyder.com>
2012-07-22 21:20:31 -0400

    working on presentatiopn

--- demo/settings.py
+++ demo/settings.py
@@ -1,4 +1,4 @@
-# Django settings for demo project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","..","static-files"))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -128,9 +128,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'demoapp',
-    'presentation'
+    'presentation',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
