2aa18c6aa44e9a8efb81505abbb3c8ad0bbe83c7
Marc Hughes <mhughes@tomsnyder.com>
2012-07-24 21:45:29 -0400

    Views & templates.

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,8 +1,15 @@
+from django.shortcuts import render_to_response, redirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+
+from models import InterestingPerson
+
 def homepage(request):
-    pass
+    return render_to_response('demoapp/homepage.html', context_instance=RequestContext(request))
 
 def people(request):
-    pass
+    people = InterestingPerson.objects.all()
+    return render_to_response('demoapp/people.html', {'people':people}, context_instance=RequestContext(request))
     
 def person(request, person_id):
     pass
